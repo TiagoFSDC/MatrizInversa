618f0008902f98af657402a83c8bc374e192fdd7
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-08 22:54:08 -0300

    Add project files.

---
+++ MatrizInversa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] mat = new int[3,3];
+        int[,] mat2 = new int[3, 3];
+
+        LerMatriz(mat);
+
+        void LerMatriz(int[,] x)
+        {
+            for (int i=0; i<3;i++)
+            {
+                for(int j = 0; j <3;j++)
+                {
+                    Console.Write($" Matriz[{i},{j}] : ");
+                    x[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 2; i >= 0; i--)
+            {
+                for (int j = 2; j >= 0; j--)
+                {
+                    Console.Write(x[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
